ec97d840be393d47a5772ad228d6145dbbea57cc
Nomey <thesavior@digitalmarauders.com>
2017-06-17 22:56:25 -0400

    changes to blank page

--- wp-content/themes/updated-yyWPtheme/blank-page.php
+++ wp-content/themes/updated-yyWPtheme/blank-page.php
@@ -25,6 +25,16 @@
 	<meta name="keywords" content="Yanging,Yangin,Shieh,Design,Brand,Branding,Print">
 </head>
 <body>
-	<?php the_content(); // Dynamic Content ?>
+	<?php
+    // TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+        <div class="entry-content-page">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+
+    <?php
+    endwhile; //resetting the page loop
+    wp_reset_query(); //resetting the page query
+    ?>
 </body>
 </html>
